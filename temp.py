56c08e33da9d46fb67474bf5100df41ad4abd5b4
samir-fawzy <sfawzy862@gmail.com>
2024-12-14 02:16:10 +0200

    add compute cost function and gradiant descient function

--- temp.py
+++ temp.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 path = 'C:\\ML\\ML-Linear-Regression\\data.txt'
 data = pd.read_csv(path,header=None,names=['Population' , 'Profit'])
 #
-#show data det/ails
+#show data details
 print('data :\n',data.head(10))
 print ('*' * 40)
 print('data describtion :\n',data.describe())
@@ -35,11 +35,51 @@ print ('*' * 40)
 x = np.matrix(x.values) # convert x 
 y = np.matrix(y.values) # convert y 
 theta = np.matrix(np.array([0,0]))
-print ('x matrix : \n',x)
-print ('x matrix shape = ',x.shape)
-print('Theta = \n',theta)
-print ('y matrix : \n',y)
-print ('y matrix shape',y.shape)
-print ('*' * 40)
-
+# print ('x matrix : \n',x)
+# print ('x matrix shape = ',x.shape)
+# print('Theta = \n',theta)
+# print ('y matrix : \n',y)
+# print ('y matrix shape',y.shape)
+# print ('*' * 40)
 # 
+# cost function
+def ComputeCost(x,y,theta):
+    # cost function = 1/2m * summition (h(x) - y)²
+    #h(x) => theta0 + theta1 * x
+    #m number of rows
+    z = np.power(((x * theta.T) - y),2)
+    #number of rows = length x
+    num_rows = len(x)
+    #summition = sum(z)
+    return sum(z) / (2 * num_rows)
+
+print ('Compute Cost = ',ComputeCost(x, y, theta))
+print ('*' * 40)
+#
+# GD => Gradiant Descent Function
+# theta = theta - alpha * derivative compute cost 
+def GradiantDescient(x,y,theta,alpha,iters):  
+    temp = np.matrix(np.zeros(theta.shape))
+    parameters = int(theta.shape[1])
+    cost = np.zeros(iters)
+    for i in range(iters):
+        error = (x * theta.T) - y
+        
+        for j in range(parameters): # 0 , 1
+            term = np.multiply(error,x[:,j])
+            # j = 0 => x = 1
+            # j = 1 => x = value
+            temp[0:j] = theta[0,j] - ((alpha / len(x)) * np.sum(term))
+        
+        theta = temp
+        cost[i] = ComputeCost(x, y, theta)
+    return theta , cost
+
+alpha = 0.01    
+iters = 1000
+
+g , cost = GradiantDescient(x,y,theta,alpha,iters)
+
+print('g : \n' , g)
+print('cost : \n' , cost[-1])
+
